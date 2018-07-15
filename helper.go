630ed6948eae019343f50f987cc80db428ec7047
Naguib Ihab <nick@driveyello.com>
2018-07-16 00:26:14 +1000

    Implement a function to find relevant articles by a tag and date

--- helper.go
+++ helper.go
@@ -1,6 +1,11 @@
 package main
 
-// Display a single article
+import (
+	"log"
+	"strings"
+)
+
+// Get a single article from array by id
 func GetArticleHelper(articles []Article, id string) Article {
 	for _, article := range articles {
 		if article.Id == id {
@@ -9,4 +14,46 @@ func GetArticleHelper(articles []Article, id string) Article {
 	}
 
 	return Article{}
+}
+
+// Get all articles that contain this date and tag
+func GetArticlesByDateAndTagHelper(articles []Article, searchForDate string, searchForTag string) TagSearchResponse {
+	result := TagSearchResponse{
+		Tag:         searchForTag,
+		Count:       0,
+		Articles:    make([]string, 0),
+		RelatedTags: make([]*Tag, 0),
+	}
+
+	articlesOnDate := getArticlesByDate(articles, searchForDate)
+	log.Println("Articles found on that date", articlesOnDate)
+	for _, article := range articlesOnDate {
+		for tagIndex, tag := range article.Tags {
+			if tag.Name == searchForTag {
+				// Increment the count
+				result.Count++
+				// Add id of article to our result
+				result.Articles = append(result.Articles, article.Id)
+				// Remove the tag we're looking for and add the other tags to related tags
+				result.RelatedTags = append(article.Tags[:tagIndex], article.Tags[tagIndex+1:]...)
+			}
+		}
+	}
+
+	return result
+}
+
+/// ---------- private functions ---------------- //
+
+// Get all articles for a particular date
+func getArticlesByDate(articles []Article, date string) []Article {
+	var results []Article
+	for _, article := range articles {
+		// formate date from yyyy-mm-dd to yyyymmdd
+		if date == strings.Replace(article.Date, "-", "", -1) {
+			results = append(results, article)
+		}
+	}
+
+	return results
 }
--- main.go
+++ main.go
@@ -18,6 +18,12 @@ type Article struct {
 	Body  string `json:"body,omitempty"`
 	Tags  []*Tag `json:"tags,omitempty"`
 }
+type TagSearchResponse struct {
+	Tag         string   `json:"tag"`
+	Count       int      `json:"count"`
+	Articles    []string `json:"articles"`
+	RelatedTags []*Tag   `json:"related_tags"`
+}
 
 var articles []Article
 
@@ -30,6 +36,12 @@ func GetArticles(w http.ResponseWriter, r *http.Request) {
 func GetArticle(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	json.NewEncoder(w).Encode(GetArticleHelper(articles, params["id"]))
+}
+
+// Display articles that are on that date and match the tag
+func GetArticlesByDateAndTag(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	json.NewEncoder(w).Encode(GetArticlesByDateAndTagHelper(articles, params["date"], params["tagName"]))
 }
 
 // Main controller
@@ -52,16 +64,27 @@ func main() {
 	articles = append(articles, Article{
 		Id:    "2",
 		Title: "stores struggle with rising demand on potato chips",
-		Date:  "2016-09-23",
+		Date:  "2016-09-22",
 		Body:  "some more text",
 		Tags: []*Tag{
 			&Tag{Name: "finance"},
 			&Tag{Name: "lifestyle"},
 		},
 	})
+	articles = append(articles, Article{
+		Id:    "3",
+		Title: "farmers planting more potatoes",
+		Date:  "2016-09-22",
+		Body:  "some more text",
+		Tags: []*Tag{
+			&Tag{Name: "health"},
+			&Tag{Name: "lifestyle"},
+		},
+	})
 
 	router.HandleFunc("/articles", GetArticles).Methods("GET")
 	router.HandleFunc("/articles/{id}", GetArticle).Methods("GET")
+	router.HandleFunc("/tags/{tagName}/{date}", GetArticlesByDateAndTag).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":"+port, router))
 	log.Print("Started running on port " + port)
